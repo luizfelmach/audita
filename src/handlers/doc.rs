fc67f62f0131a3341b2e6ea952966f97cd9b6890
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-06-30 07:35:43 -0300

    wip

--- src/handlers/doc.rs
+++ src/handlers/doc.rs
@@ -1,8 +1,12 @@
 use crate::{entity::Document, error::Result, state::AppState};
 use anyhow::Context;
 use axum::{extract::State, Json};
+use serde::{Deserialize, Serialize};
 
-pub async fn handle_doc(State(state): State<AppState>, Json(document): Json<Document>) -> Result<()> {
-    state.tx.worker.send(document).await.context("Failed to enqueue message")?;
+#[derive(Serialize, Deserialize)]
+pub struct HandleDocResquest(Document);
+
+pub async fn handle_doc(State(state): State<AppState>, Json(payload): Json<HandleDocResquest>) -> Result<()> {
+    state.tx.worker.send(payload.0).await.context("Failed to enqueue message")?;
     Ok(())
 }
