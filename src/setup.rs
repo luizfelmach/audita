7c755d97dcf15117dc069707c62809b50aa37184
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-07-04 10:36:19 -0300

    wip

--- src/setup.rs
+++ src/setup.rs
@@ -2,10 +2,8 @@ use std::env;
 use tracing::debug;
 
 pub fn logger() {
-    let level = env::var("LOG_LEVEL").unwrap_or_else(|_| "info".to_string());
-
+    let level = env::var("AUDITA_LOG_LEVEL").unwrap_or_else(|_| "info".to_string());
     env::set_var("RUST_LOG", format!("{},alloy=error,hyper=error,reqwest=error,axum=error", level));
     tracing_subscriber::fmt::init();
-
     debug!(LOG_LEVEL = level);
 }
