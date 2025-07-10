7499b2e2c87edfd0494628c1b7797687349a54e3
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-07-10 13:02:08 -0300

    reset

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,45 +1,11 @@
-import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
-import { AppSidebar } from "@/components/sidebar/app-sidebar";
-import { SiteHeader } from "@/components/sidebar/site-header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-
-import { Overview } from "./pages/overview";
-import { Search } from "./pages/search";
-import { Popes } from "./pages/popes";
-
-function DashboardLayout() {
-  return (
-    <SidebarProvider
-      style={
-        {
-          "--sidebar-width": "calc(var(--spacing) * 72)",
-          "--header-height": "calc(var(--spacing) * 12)",
-        } as React.CSSProperties
-      }
-    >
-      <AppSidebar variant="inset" />
-      <SidebarInset>
-        <SiteHeader />
-        <main className="flex-1 overflow-auto p-6">
-          <Outlet />
-        </main>
-      </SidebarInset>
-    </SidebarProvider>
-  );
-}
+import { Button } from "@/components/ui/button"
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<DashboardLayout />}>
-          <Route index element={<Overview />} />
-          <Route path="search" element={<Search />} />
-          <Route path="popes" element={<Popes />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
+  )
 }
 
-export default App;
+export default App
