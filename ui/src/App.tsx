0a6e1afb94d6486a9ea1bc9aa81272dfb879349a
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-07-11 12:55:15 -0300

    wip

--- ui/src/App.tsx
+++ ui/src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Dashboard } from "./layouts/dashboard";
+import { Overview } from "./pages/overview";
+import { Search } from "./pages/search";
+
 function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Dashboard />}>
+          <Route index element={<Overview />} />
+          <Route path="search" element={<Search />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
