fddb7b56c754eb399ad6efc314c03423f7587cb3
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-09-28 06:10:53 -0300

    wip

--- src/presentation/routes/storage.rs
+++ src/presentation/routes/storage.rs
@@ -1,11 +1,8 @@
 use crate::{
     context::Context,
-    presentation::handlers::storage::{get_hash_storage, search_documents, CacheHashStorageResponse},
+    presentation::handlers::storage::{get_hash_storage, CacheHashStorageResponse},
 };
-use axum::{
-    routing::{get, post},
-    Extension, Router,
-};
+use axum::{routing::get, Extension, Router};
 use moka::future::Cache;
 use std::{sync::Arc, time::Duration};
 
@@ -13,5 +10,5 @@ pub fn routes() -> Router<Context> {
     let cache = Cache::builder().time_to_live(Duration::from_secs(60)).max_capacity(1000).build();
     let cache: CacheHashStorageResponse = Arc::new(cache);
 
-    Router::new().route("/search", post(search_documents)).route("/hash/{id}", get(get_hash_storage)).layer(Extension(cache))
+    Router::new().route("/hash/{id}", get(get_hash_storage)).layer(Extension(cache))
 }
