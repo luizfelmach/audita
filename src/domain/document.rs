80daff451df3feec290d3ffbfb3a4687ff0da798
Luiz Felipe Machado <56140722+luizfelmach@users.noreply.github.com>
2025-09-28 09:27:59 -0300

    wip

--- src/domain/document.rs
+++ src/domain/document.rs
@@ -66,76 +66,15 @@ impl StorableDocument {
         }
     }
 
-    // Delegando para Document
-    pub fn as_firewall(&self) -> Option<&FirewallDocument> {
-        self.document.as_firewall()
-    }
-
-    pub fn as_firewall_mut(&mut self) -> Option<&mut FirewallDocument> {
-        self.document.as_firewall_mut()
-    }
-
     pub fn as_dhcp(&self) -> Option<&DhcpDocument> {
         self.document.as_dhcp()
-    }
-
-    pub fn as_dhcp_mut(&mut self) -> Option<&mut DhcpDocument> {
-        self.document.as_dhcp_mut()
-    }
-
-    pub fn as_radius(&self) -> Option<&RadiusDocument> {
-        self.document.as_radius()
-    }
-
-    pub fn as_radius_mut(&mut self) -> Option<&mut RadiusDocument> {
-        self.document.as_radius_mut()
     }
 }
 
 impl Document {
-    pub fn as_firewall(&self) -> Option<&FirewallDocument> {
-        if let Document::Firewall(ref fw) = self {
-            Some(fw)
-        } else {
-            None
-        }
-    }
-
-    pub fn as_firewall_mut(&mut self) -> Option<&mut FirewallDocument> {
-        if let Document::Firewall(ref mut fw) = self {
-            Some(fw)
-        } else {
-            None
-        }
-    }
-
     pub fn as_dhcp(&self) -> Option<&DhcpDocument> {
         if let Document::Dhcp(ref dhcp) = self {
             Some(dhcp)
-        } else {
-            None
-        }
-    }
-
-    pub fn as_dhcp_mut(&mut self) -> Option<&mut DhcpDocument> {
-        if let Document::Dhcp(ref mut dhcp) = self {
-            Some(dhcp)
-        } else {
-            None
-        }
-    }
-
-    pub fn as_radius(&self) -> Option<&RadiusDocument> {
-        if let Document::Radius(ref r) = self {
-            Some(r)
-        } else {
-            None
-        }
-    }
-
-    pub fn as_radius_mut(&mut self) -> Option<&mut RadiusDocument> {
-        if let Document::Radius(ref mut r) = self {
-            Some(r)
         } else {
             None
         }
